86ad6173cd8bebca9f09588733b2bd1739762e70
stoeck74 <cedricstoecklin@gmail.com>
2026-03-29 19:54:32 +0200

    Initial commit: Structure de base du plugin

---
+++ photoproof.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:         PhotoProof
+ * Description:         Galerie d'épreuve pour photographe
+ * Version:              0.1.0
+ * Author:               Cédric Stoecklin
+ * License:           GPL-2.0-or-later
+ * Text Domain:       mon-super-plugin
+ */
+
+// Sécurité : Empêcher l'accès direct au fichier
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+// Votre code commence ici...
